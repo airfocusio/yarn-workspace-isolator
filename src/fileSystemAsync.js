958316432a92a59e2bc3b645d7b50644209309e7
Christian Hoffmeister <mail@choffmeister.de>
2018-03-01 11:08:22 +0100

    Use new features of yarn 1.5

--- src/fileSystemAsync.js
+++ src/fileSystemAsync.js
@@ -1,5 +1,5 @@
+const copyDir = require('copy-dir')
 const fs = require('fs')
-const glob = require('glob')
 
 function readFileAsync(file) {
   return new Promise((resolve, reject) => fs.readFile(file, 'utf8', (err, res) => (!err ? resolve(res) : reject(err))))
@@ -9,22 +9,12 @@ function writeFileAsync(file, content) {
   return new Promise((resolve, reject) => fs.writeFile(file, content, err => (!err ? resolve() : reject(err))))
 }
 
-function renameAsync(file1, file2) {
-  return new Promise((resolve, reject) => fs.rename(file1, file2, (err, res) => (!err ? resolve(res) : reject(err))))
-}
-
-function unlinkAsync(file) {
-  return new Promise((resolve, reject) => fs.unlink(file, err => (!err ? resolve() : reject(err))))
-}
-
-function globAsync(pattern) {
-  return new Promise((resolve, reject) => glob(pattern, (err, res) => (!err ? resolve(res) : reject(err))))
+function copyDirAsync(from, to, filter) {
+  return new Promise((resolve, reject) => copyDir(from, to, filter, (err, res) => (!err ? resolve(res) : reject(err))))
 }
 
 module.exports = {
   readFileAsync,
   writeFileAsync,
-  renameAsync,
-  unlinkAsync,
-  globAsync,
+  copyDirAsync,
 }
--- src/runCommandAsync.js
+++ src/runCommandAsync.js
@@ -1,6 +1,41 @@
 const childProcess = require('child_process')
 
-function runCommandAsync(cwd, command, args) {
+function runCommandAsyncPipe(cwd, command, args, stdin) {
+  return new Promise((resolve, reject) => {
+    const stdout = Buffer.alloc(1024 * 1024)
+    let stdoutLength = 0
+    const stderr = Buffer.alloc(1024)
+    let stderrLength = 0
+
+    const process = childProcess.spawn(command, args, { cwd, stdio: 'pipe' })
+
+    process.stdout.on('data', chunk => {
+      chunk.copy(stdout, stdoutLength)
+      stdoutLength += chunk.length
+    })
+
+    process.stderr.on('data', chunk => {
+      chunk.copy(stderr, stderrLength)
+      stderrLength += chunk.length
+    })
+
+    process.on('close', code => {
+      if (code === 0) {
+        resolve(stdout.slice(0, stdoutLength))
+      } else {
+        reject(new Error(`exited with code ${code}:\n\n${stderr.slice(0, stderrLength).toString('utf8')}`))
+      }
+    })
+
+    if (stdin) {
+      process.stdin.write(stdin, () => process.stdin.end())
+    } else {
+      process.stdin.end()
+    }
+  })
+}
+
+function runCommandAsyncInherit(cwd, command, args) {
   return new Promise((resolve, reject) => {
     const process = childProcess.spawn(command, args, { cwd, stdio: 'inherit' })
 
@@ -14,4 +49,7 @@ function runCommandAsync(cwd, command, args) {
   })
 }
 
-module.exports = runCommandAsync
+module.exports = {
+  runCommandAsyncPipe,
+  runCommandAsyncInherit,
+}
